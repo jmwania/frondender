a6723e6782f4f2b798be1c86b135524466f7486b
jmwania <josphatmwania237@gmail.com>
2019-04-22 17:10:01 +0300

    [20192204]

---
+++ images/js/script.js
@@ -0,0 +1,12 @@
+<p id="demo"></p>
+
+<script>
+function myFunction() {
+  var inpObj = document.getElementById("id1");
+  if (!inpObj.checkValidity()) {
+    document.getElementById("demo").innerHTML = inpObj.validationMessage;
+  } else {
+    document.getElementById("demo").innerHTML = "Input OK";
+  } 
+} 
+</script>
