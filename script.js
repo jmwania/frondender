6f6c4b8866ba3c4f16d05ab672e86912128d377c
jmwania <josphatmwania237@gmail.com>
2019-04-29 17:12:10 +0300

    [20190429 JS modified]

---
+++ script.js
@@ -0,0 +1,18 @@
+var btt = document.getElementById("back-to-top"),
+ body = document.body,
+ docElem = document.documentElement,
+ offset = 100,
+ scrollPos, docHeight;
+
+ // Calculate the document height
+ docHeight = Math.max(body.scrollHeight, body.offsetHeight, docElem.clientHeight, docElem.scrollHeight, docElem.offsetHeight);
+ if (docHeight != 'undefined') {
+     offset = docHeight / 4;
+ }
+
+ // Add scroll event listener 
+
+ window.addEventListener("scroll", function(event) {
+     scrollPos = body.scrollTop || docElem.scrollTop;
+     btt.className = (scrollPos > offset) ? "visible"? : ""; 
+ });
