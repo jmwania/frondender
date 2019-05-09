836abd134ba33879abb89f90d3a7c9544067cc8d
jmwania <josphatmwania237@gmail.com>
2019-05-09 11:25:59 +0300

    [20190805]

--- script.js
+++ script.js
@@ -22,3 +22,39 @@ btt.addEventListener("click", function(event) {
     
 })
  });
+/*
+* - define variables
+* Focus the input
+*/
+
+ var targetInput = document.getElementById( "county"), 
+    results = document.getElementById( "autocomplete-results"), 
+    countyList = ['Mombasa', 'Tata Taveta', 'Nairobi', 'Nakuru', 'Nyeri', 'Siaya', 'Kisumu', 'Kitui', 'Machakos', 'Makueni'], matches = [];
+
+// Focus the input
+targetInput.focus();
+
+// Add event listener for the iinput keyup
+targetInput.addEventListener( "keyup", function( event ) {
+    /*
+    * Key codes
+    *
+    * Enter: 13
+    * Arrow up: 38
+    * Arrow down: 40
+    */
+   results.innerHTML ="";
+   toggleResults( "hide" );
+   if ( this.value.length > 0)
+       matches = getMatches( this.value);
+});
+
+// define a function for toggling the results list
+function toggleResults ( action ) {
+        if ( action == "show" ) {
+           results.classList.add( "visible" );
+        } else if ( action == "hide" ) {
+            results.classList.remove("visible");
+        }
+   }
+})
